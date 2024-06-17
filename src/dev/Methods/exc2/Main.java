0119c2ce97aafa2f04965bb9f633453f454ad84b
Nikolay Yakimov <nyakimov123@gmail.com>
2024-06-17 14:42:24 +0300

    Exc 2 done

--- src/dev/Methods/exc2/Main.java
+++ src/dev/Methods/exc2/Main.java
@@ -16,7 +16,7 @@ public class Main {
         if(grade<2.00 ||grade >6.00){
             System.out.println("Error enter valid grade!");
         }
-        if(grade>2.00&&grade<=2.99)
+        if(grade>=2.00&&grade<=2.99)
             System.out.println("Fail");
         else if (grade>=3.00&&grade<=3.49) {
             System.out.println("Poor");
